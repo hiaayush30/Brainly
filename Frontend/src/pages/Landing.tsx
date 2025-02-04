847364b1d8fa61416579f8f2fcb44c9537292e8a
aayush <hiaayush30@gmail.com>
2025-02-05 01:37:19 +0530

    added responsiveness

--- Frontend/src/pages/Landing.tsx
+++ Frontend/src/pages/Landing.tsx
@@ -6,11 +6,11 @@ const Landing = () => {
         <div style={{backgroundImage:'url("bg.jpeg")'}}
         className='bg-cover min-h-screen flex flex-col items-center justify-center'>
             <div className='bg-[rgba(0,0,0,0.8)] flex flex-col items-center px-3 rounded-lg'>
-                <h1 className='text-[172px] mb-0 pb-0 text-blue-600'
+                <h1 className='max-sm:text-[85px] text-[172px] mb-0 pb-0 text-blue-600'
                 >Bra<span className='text-slate-200'>i</span>nly</h1>
                 <p className='mb-5 text-lg text-white'>Your Second Brain</p>
             </div>
-            <div>
+            <div className='max-sm:mt-10 mt-5'>
                 <button className='hover:bg-blue-500 cursor-pointer text-white px-2 bg-blue-600 rounded-md p-1 m-2' onClick={() => navigate('/login')}>Login</button>
                 <button className='hover:bg-blue-500 cursor-pointer text-white px-2 bg-blue-600 rounded-md p-1 m-2' onClick={() => navigate('/signup')}>Signup</button>
             </div>
