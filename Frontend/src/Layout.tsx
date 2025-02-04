1c757735ff6574c2efb032b3284ca2910f634f1e
aayush <hiaayush30@gmail.com>
2025-02-05 01:21:02 +0530

    added vercel analytics

--- Frontend/src/Layout.tsx
+++ Frontend/src/Layout.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import Sidebar from './components/Sidebar'
-
+import { Analytics } from "@vercel/analytics/react"
 const Layout = () => {
   return (
     <div className='flex w-screen min-h-screen bg-slate-300'>
       <Sidebar/>
       <div className='max-sm:w-full w-[75%]'>
          <Outlet/>
+         <Analytics/>
       </div>
     </div>
   )
