f5b3f24be29b9ed2dae935ed60e4dca3d09896ac
aayush <hiaayush30@gmail.com>
2025-02-05 11:42:33 +0530

    added title for mobile screens

--- Frontend/src/components/AllNotes/Header.tsx
+++ Frontend/src/components/AllNotes/Header.tsx
@@ -6,32 +6,40 @@ import AddContent from "../../popup/AddContent";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleAddContent, toggleShareBrain } from "../../redux/features/service/serviceSlice";
 import { RootState } from "../../redux/store";
+import { LuBrain } from "react-icons/lu";
 
 const Header = () => {
-    const {openAddContent,openShareBrain} = useSelector((store:RootState)=>store.service);
-    const dispatch=useDispatch();
+    const { openAddContent, openShareBrain } = useSelector((store: RootState) => store.service);
+    const dispatch = useDispatch();
     return (
-        <div className="flex w-full justify-between px-10 py-6 items-center">
-            {/* <RxHamburgerMenu className="absolute top-1 left-1 sm:hidden cursor-pointer text-2xl" /> */}
-            <div className="flex items-center gap-3">
-                <h2 className="text-3xl font-medium"
-                >All Notes</h2>
-                <FaBookBookmark className="text-2xl text-blue-800" />
+        <div>
+            <div className="sm:hidden flex justify-center items-center">
+                <LuBrain className='text-blue-800 text-5xl' />
+                <h1 className={`relative text-center m-3 text-3xl font-semibold cursor-pointer hover:scale-105 transition-all duration-500`}
+                >Brainly</h1>
             </div>
-            <div className="flex gap-5 p-2 max-sm:gap-2">
-                <button onClick={() => dispatch(toggleShareBrain(true))}
-                    className='cursor-pointer hover:bg-blue-500 transition-all flex items-center gap-1 px-2 py-1 bg-blue-700 text-white rounded-lg'>
-                    <BsShare />
-                    <p className="max-sm:hidden">Share Brain</p>
-                </button>
-                <button onClick={() => dispatch(toggleAddContent(true))}
-                className="cursor-pointer hover:bg-blue-500 transition-all flex items-center gap-1 px-2 py-1 bg-blue-700 text-white rounded-lg">
-                    <FaPlus />
-                    <p className="max-sm:hidden">Add Content</p>
-                </button>
+            <div className="flex w-full justify-between px-10 py-6 items-center">
+                {/* <RxHamburgerMenu className="absolute top-1 left-1 sm:hidden cursor-pointer text-2xl" /> */}
+                <div className="flex items-center gap-3">
+                    <h2 className="text-3xl font-medium"
+                    >All Notes</h2>
+                    <FaBookBookmark className="text-2xl text-blue-800" />
+                </div>
+                <div className="flex gap-5 p-2 max-sm:gap-2">
+                    <button onClick={() => dispatch(toggleShareBrain(true))}
+                        className='cursor-pointer hover:bg-blue-500 transition-all flex items-center gap-1 px-2 py-1 bg-blue-700 text-white rounded-lg'>
+                        <BsShare />
+                        <p className="max-sm:hidden">Share Brain</p>
+                    </button>
+                    <button onClick={() => dispatch(toggleAddContent(true))}
+                        className="cursor-pointer hover:bg-blue-500 transition-all flex items-center gap-1 px-2 py-1 bg-blue-700 text-white rounded-lg">
+                        <FaPlus />
+                        <p className="max-sm:hidden">Add Content</p>
+                    </button>
+                </div>
+                {openShareBrain && <ShareBrain />}
+                {openAddContent && <AddContent />}
             </div>
-            {openShareBrain && <ShareBrain/>}
-            {openAddContent && <AddContent/>}
         </div>
     )
 }
